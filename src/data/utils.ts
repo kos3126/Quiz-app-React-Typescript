944d3453bda89794429016a6f3e334964e89632e
KosM <kosukemorimoto1@gmail.com>
2021-05-31 10:23:53 +0900

    added comments

--- src/data/utils.ts
+++ src/data/utils.ts
@@ -2,3 +2,15 @@ export const shuffleArray = (array: any[]) => {
   // copy the array and sort them
   return [...array].sort(() => Math.random() - 0.5);
 };
+
+/*
+--- Fisher–Yates shuffle ---
+
+const shuffle = ([...array]) => {
+  for (let i = array.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+*/
